d05a33b2e99353ac72330ecdfdc8e7cffeec335d
Snarevip7010 <naoto1nose@gmail.com>
2021-02-06 23:38:45 +0900

    デプロイ準備

--- DCWP/settings.py
+++ DCWP/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'mkka29)lzug6=m+#r=qz4824c4&*4eue%vpb$04!b(za%$@l!u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
