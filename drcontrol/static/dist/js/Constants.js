eed43028c17bd83920a8604e18f466490479fd53
Snarevip7010 <naoto1nose@gmail.com>
2021-02-22 22:10:04 +0900

    ファイルダウンロード機能追加

---
+++ drcontrol/static/dist/js/Constants.js
@@ -0,0 +1,23 @@
+Constans = {
+    title:{
+        controlstatus: {
+            selectnone: '99',
+            takeof: '1',
+            land: '2',
+            forward: '3',
+            back: '4',
+            right: '5',
+            left: '6',
+            delay: '0'
+        },
+        command: {
+            TAKEOF: 'takeof',
+            LAND: 'lamd',
+            FORWARD: 'forward',
+            BACK: 'back',
+            RIGHT: 'right',
+            LEFT: 'left',
+            DELAY: 'delay'
+        },
+    }
+}
--- drcontrol/static/dist/js/dronecontrol.js
+++ drcontrol/static/dist/js/dronecontrol.js
@@ -1,31 +1,71 @@
 /*
 　ドローンコマンド入力画面
 */
-
-
-var options = [
-  ['20','30','40','50','60','70','80','90','100','200','300','400','500'],
-  ['5', '10', '15','20'],
-];
+debugger;
 
 //セレクトボックス変更時
 $('#SelectCommand').change(function() {
  
-    console.log("OK")
     var command = $('#SelectCommand').val();
-    
     console.log(command);
     
-    var select_cm = $('SelectCommand')
-    var select_op = $('SelectOption')
-    var selected_cm = select_cm.val();
-    
-    $('#SelectOption option').remove();
-    
-    if(selected_cm != '0' && selected_cm != '99'){
+    //着陸・離陸字はオプションを非活性にする
+    if(command == '1' || command == '2'){
+      $('#SelectOption').prop("disabled", true);
+      $('#sec').hide();
+      $('#cm').hide();  
+    }
+    else {
+      $('#SelectOption').prop("disabled", false);
+      
+      //未選択・待機以外の場合単位(cm)を表示
+      if(command != '0' && command != '99'){
+        $('#sec').hide();  
         $('#cm').show();
+      }
+      //待機の場合単に(秒)を表示
+      else if(command == '0'){
+        $('#cm').hide();
+        $('#sec').show();
+      }
+      else{
+        $('#sec').hide();
+        $('#cm').hide();
+      }
     }
 
 
 })
-    
+ 
+$("#downloadBtn").on('click', function(){
+  console.log("clicked");
+
+  
+  downloadPyFile();
+
+});
+
+function downloadPyFile(){
+  var command = $("#SelectCommand").val();
+  var commandOpt = $("#SelectOption").val();
+  console.log(commandOpt);
+ // 配列 の用意
+ var array_data = [[command, commandOpt]];
+
+ // BOM の用意（文字化け対策）
+ var bom = new Uint8Array([0xEF, 0xBB, 0xBF]);  
+
+ // CSV データの用意
+ var command_data = array_data.map(function(l){return l.join(' ')}).join('\r\n');
+
+ var blob = new Blob([bom, command_data], { type: 'text/txt' });
+
+ var url = (window.URL || window.webkitURL).createObjectURL(blob);
+
+ var a = document.getElementById('downloader');
+ a.download = 'command.txt';
+ a.href = url;
+
+ // ダウンロードリンクをクリックする
+ $('#downloader')[0].click();
+}
