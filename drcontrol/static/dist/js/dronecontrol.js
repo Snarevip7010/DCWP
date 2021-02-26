7915aa856b880e624618d972d879b608dfac1f96
一瀬　直人 <ichinose@seed-sys.co.jp>
2021-02-26 17:14:16 +0900

    フォーム追加処理作成

--- drcontrol/static/dist/js/dronecontrol.js
+++ drcontrol/static/dist/js/dronecontrol.js
@@ -2,7 +2,7 @@
 　ドローンコマンド入力画面
 */
 debugger;
-
+//全般見直し複数のフォームができるのでリストで回さないとダメ。
 //セレクトボックス変更時
 $('#SelectCommand').change(function() {
  
@@ -11,12 +11,12 @@ $('#SelectCommand').change(function() {
     
     //着陸・離陸字はオプションを非活性にする
     if(command == '1' || command == '2'){
-      $('#SelectOption').prop("disabled", true);
+      $('#InputOption').prop("disabled", true);
       $('#sec').hide();
       $('#cm').hide();  
     }
     else {
-      $('#SelectOption').prop("disabled", false);
+      $('#InputOption').prop("disabled", false);
       
       //未選択・待機以外の場合単位(cm)を表示
       if(command != '0' && command != '99'){
@@ -36,6 +36,28 @@ $('#SelectCommand').change(function() {
 
 
 })
+//form追加処理
+$("#addButton").on('click', function(){
+  console.log("clicked");
+  var tr_form = '' +
+    '<div class="row" style="display: flex;"><tr>' +
+    '<td><select id="SelectCommand[]" class="form-control col-6" style="margin-top:30px;">' +
+    '<option value=99>コマンドを選択</option>' +
+    '<option value=1>離陸</option>' +
+    '<option value=2>着陸</option>' +
+    '<option value=3>前進</option>' +
+    '<option value=4>後退</option>' +
+    '<option value=5>右へ</option>' +
+    '<option value=6>左へ</option>' +
+    '<option value=0>待機</option>' +
+    '</select></td>' +
+    '<td><input type="text" id="InputOption[2]" class="form-control col-5" style="margin-top:30px;">' +
+    '<p id="cm" style="display :none; margin: 40px 0px 0px 0px;" >(cm)</p>' +
+    '<p id="sec" style="display : none; margin: 40px 0px 0px 0px;" >(秒)</p></td>' +
+  '</tr></div>';
+
+  $(tr_form).appendTo($('#commandLine'));
+});
  
 $("#downloadBtn").on('click', function(){
   console.log("clicked");
@@ -47,7 +69,7 @@ $("#downloadBtn").on('click', function(){
 
 function downloadPyFile(){
   var command = $("#SelectCommand").val();
-  var commandOpt = $("#SelectOption").val();
+  var commandOpt = $("#InputOption").val();
   console.log(commandOpt);
  // 配列 の用意
  var array_data = [[command, commandOpt]];
