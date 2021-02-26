65d956804b4cb5ee1503052b659e4da8d1a4055b
Snarevip7010 <naoto1nose@gmail.com>
2021-02-27 02:44:10 +0900

    フォーム複数化対応

--- drcontrol/static/dist/js/dronecontrol.js
+++ drcontrol/static/dist/js/dronecontrol.js
@@ -2,46 +2,68 @@
 　ドローンコマンド入力画面
 */
 debugger;
-//全般見直し複数のフォームができるのでリストで回さないとダメ。
+
+var seq = parseInt(0);
+
+//サイドバー遷移処理
+$(document).on('click', '[data-toggle="push-menu"]', function () {
+  if (!$('body').hasClass("sidebar-collapse") && $('body').hasClass("sidebar-open")) {
+      $('body').removeClass("sidebar-open");
+      $('body').addClass("sidebar-collapse");
+      $('body').addClass("sidebar-mini");
+  //console.log('menu kapa');
+  } else if($('body').hasClass("sidebar-collapse")) {
+      $('body').removeClass("sidebar-collapse");
+      $('body').addClass("sidebar-open");
+  //console.log('menu ac');
+  } else {
+      $('body').addClass("sidebar-collapse");
+      $('body').addClass("sidebar-open");
+  }
+});
+
+
 //セレクトボックス変更時
-$('#SelectCommand').change(function() {
- 
-    var command = $('#SelectCommand').val();
+function changeCommand(){
+  var list = $("#commandLine select").length;
+  for(var i=0; i<list; i++ ){
+
+    var command = $('#SelectCommand'+[i]).val();
     console.log(command);
     
     //着陸・離陸字はオプションを非活性にする
     if(command == '1' || command == '2'){
-      $('#InputOption').prop("disabled", true);
+      $('#InputOption' +[i]).prop("disabled", true);
       $('#sec').hide();
       $('#cm').hide();  
     }
     else {
-      $('#InputOption').prop("disabled", false);
+      $('#InputOption' +[i]).prop("disabled", false);
       
       //未選択・待機以外の場合単位(cm)を表示
       if(command != '0' && command != '99'){
-        $('#sec').hide();  
-        $('#cm').show();
+        $('#sec'+[i]).hide();  
+        $('#cm'+[i]).show();
       }
       //待機の場合単に(秒)を表示
       else if(command == '0'){
-        $('#cm').hide();
-        $('#sec').show();
+        $('#cm'+[i]).hide();
+        $('#sec'+[i]).show();
       }
       else{
-        $('#sec').hide();
-        $('#cm').hide();
+        $('#sec'+[i]).hide();
+        $('#cm'+[i]).hide();
       }
     }
-
-
-})
+  }
+}
 //form追加処理
 $("#addButton").on('click', function(){
   console.log("clicked");
+  var idseq = seq+parseInt(1);
   var tr_form = '' +
     '<div class="row" style="display: flex;"><tr>' +
-    '<td><select id="SelectCommand[]" class="form-control col-6" style="margin-top:30px;">' +
+    '<td><select id="SelectCommand'+ idseq +'" class="form-control col-6" style="margin-top:30px;" onchange="changeCommand()">' +
     '<option value=99>コマンドを選択</option>' +
     '<option value=1>離陸</option>' +
     '<option value=2>着陸</option>' +
@@ -51,34 +73,34 @@ $("#addButton").on('click', function(){
     '<option value=6>左へ</option>' +
     '<option value=0>待機</option>' +
     '</select></td>' +
-    '<td><input type="text" id="InputOption[2]" class="form-control col-5" style="margin-top:30px;">' +
-    '<p id="cm" style="display :none; margin: 40px 0px 0px 0px;" >(cm)</p>' +
-    '<p id="sec" style="display : none; margin: 40px 0px 0px 0px;" >(秒)</p></td>' +
+    '<td><input type="text" id="InputOption'+ idseq +'" class="form-control col-5" style="margin-top:30px;">' +
+    '<p id="cm'+ idseq +'" style="display :none; margin: 40px 0px 0px 0px;" >(cm)</p>' +
+    '<p id="sec'+ idseq +'" style="display : none; margin: 40px 0px 0px 0px;" >(秒)</p></td>' +
   '</tr></div>';
 
   $(tr_form).appendTo($('#commandLine'));
+  seq = idseq;
 });
  
 $("#downloadBtn").on('click', function(){
-  console.log("clicked");
-
-  
   downloadPyFile();
-
 });
 
 function downloadPyFile(){
-  var command = $("#SelectCommand").val();
-  var commandOpt = $("#InputOption").val();
-  console.log(commandOpt);
- // 配列 の用意
- var array_data = [[command, commandOpt]];
-
+  var list = $("#commandLine select").length;
+  var array_temp = [];
+  for(var i=0; i<list; i++ ){
+    var command = $("#SelectCommand" +[i]).val();
+    var commandOpt = $("#InputOption" +[i]).val();
+    array_temp.push(command, " ", commandOpt, '\r\n');
+    console.log(array_temp);
+  }
+  var array_data = [array_temp];
  // BOM の用意（文字化け対策）
  var bom = new Uint8Array([0xEF, 0xBB, 0xBF]);  
 
  // CSV データの用意
- var command_data = array_data.map(function(l){return l.join(' ')}).join('\r\n');
+ var command_data = array_data.map(function(l){return l.join('')}).join('\r\n');
 
  var blob = new Blob([bom, command_data], { type: 'text/txt' });
 
@@ -90,4 +112,5 @@ function downloadPyFile(){
 
  // ダウンロードリンクをクリックする
  $('#downloader')[0].click();
+
 }
